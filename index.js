27b9cbf09f0b1f62a89ad080420147809d4558f5
Lucas Beneston <benestonlucas@gmail.com>
2020-05-15 01:04:45 +0200

    Ajout default images

--- index.js
+++ index.js
@@ -14,22 +14,30 @@ function entierAleatoire(min, max){
 
 (async () => {
     let button = document.getElementById('randomButton')
-    console.log(button)
-
     button.addEventListener('click', async() => {
-        const id = entierAleatoire(1, 1000)
+        const id = entierAleatoire(1, 2000)
+            let movie = await fetchMovie(id);
 
-        let movie = await fetchMovie(id);
-        document.getElementById("resultRandom").innerHTML = `
-        <div class="resultRandom-illustration">
-              <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}">
-        </div>
-        <div class="resultRandom-description">
-            <h1 class="resultRandom-description-heading">${movie.original_title}</h1>
-            <p class="resultRandom-description-overview">${movie.overview}</p>
-            <p class="resultRandom-description-runtime"> Runtime : ${movie.runtime} minutes</p>
-            <p class="resultRandom-description-releaseDate"> Release date : ${movie.release_date}</p>
-            <p class="resultRandom-description-voteAverage"> Vote average : ${movie.vote_average}/10 <span>(${movie.vote_count} votes)</span></p>
-        </div>`;
+            if (movie.poster_path !== undefined){
+            document.getElementById("resultRandom").innerHTML = `
+            <div class="resultRandom-illustration">
+                  <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}">
+            </div>
+            <div class="resultRandom-description">
+                <h1 class="resultRandom-description-heading">${movie.original_title}</h1>
+                <p class="resultRandom-description-overview">${movie.overview}</p>
+                <p class="resultRandom-description-runtime"> Runtime : ${movie.runtime} minutes</p>
+                <p class="resultRandom-description-releaseDate"> Release date : ${movie.release_date}</p>
+                <p class="resultRandom-description-voteAverage"> Vote average : ${movie.vote_average}/10 <span>(${movie.vote_count} votes)</span></p>
+            </div>`;
+            } else {
+                document.getElementById("resultRandom").innerHTML = `
+                <div class="resultRandom-illustration">
+                      <img src="default${entierAleatoire(1, 4)}.png">
+                </div>
+                <div class="resultRandom-description">
+                    <h1 class="resultRandom-description-heading">Sorry, this movie is gone. I think it was too bad.</h1>
+                </div>`;
+            }
     })
 })();
