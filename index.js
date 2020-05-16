56530e745db551141fdb1f6eeec4a0c7dc7b1e18
Lucas Beneston <benestonlucas@gmail.com>
2020-05-16 21:49:00 +0200

    Début modal youtube

--- index.js
+++ index.js
@@ -8,36 +8,73 @@ async function fetchMovie(movieId) {
   return movie;
 }
 
+// async function fetchVideoMovie(movieId){
+//     const url = `${API_URL}${movieId}/videos?api_key=${API_KEY}`;
+//     let res = await fetch(url);
+//     let video = await res.json();
+//     return video.results[0].key;
+// }
+
+
 function entierAleatoire(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
    }
 
 (async () => {
-    let button = document.getElementById('randomButton')
-    button.addEventListener('click', async() => {
-        const id = entierAleatoire(1, 2000)
-            let movie = await fetchMovie(id);
+    let button = document.querySelector('.randomButton')
+    let trailerButton = document.querySelector('.trailerButton')
+    let alreadySeenButton = document.querySelector('.alreadySeenButton')
 
-            if (movie.poster_path !== undefined){
-            document.getElementById("resultRandom").innerHTML = `
-            <div class="resultRandom-illustration">
-                  <img src="https://image.tmdb.org/t/p/original/${movie.poster_path}">
-            </div>
-            <div class="resultRandom-description">
-                <h1 class="resultRandom-description-heading">${movie.original_title}</h1>
-                <p class="resultRandom-description-overview">${movie.overview}</p>
-                <p class="resultRandom-description-runtime"> Runtime : ${movie.runtime} minutes</p>
-                <p class="resultRandom-description-releaseDate"> Release date : ${movie.release_date}</p>
-                <p class="resultRandom-description-voteAverage"> Vote average : ${movie.vote_average}/10 <span>(${movie.vote_count} votes)</span></p>
-            </div>`;
-            } else {
-                document.getElementById("resultRandom").innerHTML = `
-                <div class="resultRandom-illustration">
-                      <img src="default${entierAleatoire(1, 4)}.png">
-                </div>
-                <div class="resultRandom-description">
-                    <h1 class="resultRandom-description-heading">Sorry, this movie is gone. I think it was too bad.</h1>
-                </div>`;
-            }
+    // EVENT : Pour random les films au click sur le bouton "randomButton"
+    button.addEventListener('click', async() => {
+    const id = entierAleatoire(1, 2000) // Mettre entre 1 et lenght des id pour avoir la totalité des films
+    let movie = await fetchMovie(id);
+
+        if (movie.poster_path !== undefined){
+            document.querySelector('.resultRandom-container-illustration').innerHTML = `<img class="resultRandom-illustration" src="https://image.tmdb.org/t/p/original/${movie.poster_path}">`
+            document.querySelector('.resultRandom-description-heading').innerHTML = `${movie.original_title} <span>(${movie.release_date.substr(0, 4)})</span>`
+            document.querySelector('.resultRandom-description-overview').innerHTML = `${movie.overview}`
+            document.querySelector('.resultRandom-description-runtime').innerHTML = `Runtime : ${movie.runtime} minutes`
+            document.querySelector('.resultRandom-description-voteAverage').innerHTML = `Vote average : ${movie.vote_average}/10 <span>(${movie.vote_count} votes)</span>`
+
+            // afficher les boutons
+            alreadySeenButton.style.display = "block"
+            trailerButton.style.display = "block"
+        } else {
+            // Trouver un film qui est valide
+            console.log("Pas de film")
+
+            // A Retirer plus tard
+            document.querySelector('.resultRandom-container-illustration').innerHTML = `<img class="resultRandom-illustration" src="/default${entierAleatoire(1, 4)}.png">`
+            document.querySelector('.resultRandom-description-heading').innerHTML = `This film is over...`
+            document.querySelector('.resultRandom-description-overview').innerHTML = ``
+            document.querySelector('.resultRandom-description-runtime').innerHTML = ``
+            document.querySelector('.resultRandom-description-voteAverage').innerHTML = ``
+            alreadySeenButton.style.display = ""
+            trailerButton.style.display = ""
+        }
+
+    // let video = await fetchVideoMovie(id)
+
     })
+
+
+    // EVENT : Pour afficher la modal avec la bande-annonce Youtube lors d'un click sur le bouton "trailerButton"
+    let modalTrailerStatus = true
+    let modaleYoutube = document.getElementById('modalYoutube')
+
+    trailerButton.addEventListener('click', () => {
+        if (modalTrailerStatus == true){
+            console.log("Ouverture modale trailer")
+            modaleYoutube.style.display = "block"
+
+            modalTrailerStatus = false
+        } else {
+            console.log("Fermeture modale trailer")
+            modaleYoutube.style.display = ""
+
+            modalTrailerStatus = true
+        }
+    })
+
 })();
